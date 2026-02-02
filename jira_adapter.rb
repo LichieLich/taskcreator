07fc9b6dd6295a08d628c670363281d678e4a6b6
kolkovss <kolkovss@it.mos.ru>
2026-02-02 17:02:49 +0300

    Updated for new issue type

--- jira_adapter.rb
+++ jira_adapter.rb
@@ -23,22 +23,37 @@ class JiraAdapter
   def create_task(test_case)
     issue = @client.Issue.build
 
-
     test_case.name.gsub!('  ', ' ')
     test_case.name = test_case.name[0..180]
 
-    issue.save({
-                 fields: {
-                   project: { id: 13_420 },
-                   summary: "#{test_case.id} #{test_case.name}",
-                   description: "#{$params['testit']['host']}projects/#{$params['testit']['project']}/tests/#{test_case.id}",
-                   issuetype: { id: 10_002 },
-                   customfield_10100: $params['jira']['epic_key'],
-                   labels: ['автотест'],
-                   components: [{ name: $params['jira']['component'] }],
-                   assignee: nil
-                 }
-               })
+    if $params['jira']['issue_type'] == 10_905
+      issue.save({
+                   fields: {
+                     project: { id: 13_420 },
+                     summary: "#{test_case.id} #{test_case.name}",
+                     description: "#{$params['testit']['host']}projects/#{$params['testit']['project']}/tests/#{test_case.id}",
+                     issuetype: { id: $params['jira']['issue_type'] },
+                     customfield_10100: $params['jira']['epic_key'],
+                     customfield_15747: { id: ($params['jira']['type_of_work']).to_s, disabled: false },
+                     labels: ['автотест'],
+                     components: [{ name: $params['jira']['component'] }],
+                     assignee: nil
+                   }
+                 })
+    else
+      issue.save({
+                   fields: {
+                     project: { id: 13_420 },
+                     summary: "#{test_case.id} #{test_case.name}",
+                     description: "#{$params['testit']['host']}projects/#{$params['testit']['project']}/tests/#{test_case.id}",
+                     issuetype: { id: $params['jira']['issue_type'] },
+                     customfield_10100: $params['jira']['epic_key'],
+                     labels: ['автотест'],
+                     components: [{ name: $params['jira']['component'] }],
+                     assignee: nil
+                   }
+                 })
+    end
   end
 
   def find_task_key_by_summary(summary)
